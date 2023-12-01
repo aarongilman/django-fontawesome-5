61576ebac125756ea6bfc28b4df7cebb8628e088
François GUÉRIN <fguerin@ville-tourcoing.fr>
2023-12-01 11:21:58 +0100

    Add icon_as_html inclusion tag and adjust static file loading
    Introduced an inclusion tag, "icon_as_html", to allow better customization of the FontAwesome icons in templating. Loading of the static files has been adjusted for better script organization. The ordering of the imported modules has been changed for better readability following PEP8. Reorganization of the raw code was made for better readability and easier maintenance of the code moving forward. Furthermore, the function "icon_as_html" has been outfitted with better typing and documentation for better understanding of the function's capability. The entirety of these changes improves the overall functionality and readability of the code, leading to better user experience when employing django-fontawesome-5.

--- fontawesome_5/templatetags/fontawesome_5.py
+++ fontawesome_5/templatetags/fontawesome_5.py
@@ -1,13 +1,12 @@
-from __future__ import unicode_literals
+from typing import Any, Optional
 
 from django import template
 from django.conf import settings
 from django.templatetags.static import static
-from django.utils.html import format_html, mark_safe, conditional_escape
+from django.utils.html import conditional_escape, format_html, mark_safe
 
 from .. import Icon
 from ..app_settings import get_css
-
 
 css = get_css()
 register = template.Library()
@@ -23,11 +22,42 @@ def fontawesome_5_static():
     staticfiles = []
 
     for stylesheet in css:
-        staticfiles.append(format_html(
-            '<link href="{}" rel="stylesheet" media="all">', stylesheet))
+        staticfiles.append(format_html('<link href="{}" rel="stylesheet" media="all">', stylesheet))
 
-    staticfiles.append(format_html(
-        '<script type="text/javascript" src="{}"></script>', static('fontawesome_5/js/django-fontawesome.js')
-    ))
+    staticfiles.append(
+        format_html(
+            '<script type="text/javascript" src="{}"></script>', static("fontawesome_5/js/django-fontawesome.js")
+        )
+    )
 
-    return mark_safe(conditional_escape('\n').join(staticfiles))
+    return mark_safe(conditional_escape("\n").join(staticfiles))
+
+
+@register.inclusion_tag("fontawesome_5/icon_as_html.html")
+def icon_as_html(icon: str, size: Optional[str] = None, title: Optional[str] = None) -> dict[str, Any]:
+    """
+    Render the icon as HTML, with extra size and title.
+
+    :param icon: Icon
+    :param size: Size of the icon
+    :param title: Title of the icon
+    :return: Render-able dict
+    """
+    output = {"debug": settings.DEBUG}
+    # Parse the icon
+    split = icon.split(",")
+    if len(split) == 1:
+        _prefix = settings.FONTAWESOME_5_PREFIX
+        _icon_name = split[0]
+    else:
+        _prefix = split[0]
+        _icon_name = split[1]
+    output.update(
+        {
+            "title": title,
+            "size": size,
+            "icon": _icon_name,
+            "style_prefix": _prefix,
+        }
+    )
+    return output
