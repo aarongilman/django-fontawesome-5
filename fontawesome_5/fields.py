19846974be80ac3476849ead87ef60efcea4275d
François GUÉRIN <fguerin@ville-tourcoing.fr>
2024-06-10 10:28:33 +0200

    Use pyproject.toml file for wheel creation Add a dependency to hatchling as build engine

--- fontawesome_5/fields.py
+++ fontawesome_5/fields.py
@@ -1,36 +1,33 @@
-from __future__ import absolute_import
-
 from django.db import models
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext as _
 
 from . import Icon
 from .app_settings import get_prefix
 from .forms import IconFormField
 from .shims import shims
 
-
 prefix = get_prefix()
 
 
 class IconField(models.Field):
-    description = _('A fontawesome icon field')
+    description = _("A fontawesome icon field")
 
     def __init__(self, *args, **kwargs):
-        kwargs['max_length'] = 60
-        kwargs['blank'] = True
+        kwargs["max_length"] = 60
+        kwargs["blank"] = True
         super(IconField, self).__init__(*args, **kwargs)
 
     def get_internal_type(self):
-        return 'CharField'
+        return "CharField"
 
     def from_db_value(self, value, expression, connection, context):
         if value is None:
             return value
-        if not ',' in value:
+        if "," not in value:
             value = shims.get(value, None)
         if value is None:
             return value
-        values = value.split(',')
+        values = value.split(",")
         return Icon(name=values[1], style_prefix=values[0], prefix=prefix)
 
     def to_python(self, value):
@@ -38,19 +35,19 @@ class IconField(models.Field):
             return value
         if value is None:
             return value
-        if not ',' in value:
+        if "," not in value:
             value = shims.get(value, None)
-        if not value or value == 'None':
+        if not value or value == "None":
             return None
-        values = value.split(',')
-        return Icon( name=values[1], style_prefix=values[0], prefix=prefix)
+        values = value.split(",")
+        return Icon(name=values[1], style_prefix=values[0], prefix=prefix)
 
     def get_prep_value(self, value):
         return str(value)
 
     def formfield(self, **kwargs):
         defaults = {
-            'form_class': IconFormField,
+            "form_class": IconFormField,
         }
 
         defaults.update(kwargs)
